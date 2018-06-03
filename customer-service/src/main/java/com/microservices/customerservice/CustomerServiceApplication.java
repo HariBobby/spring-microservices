7cd0fb711020a3cc7beca13343c7ec9750502bea
HariBobby <hari.polnati@gmail.com>
2018-06-03 09:23:13 +0530

    Configured eureka server and ribbon

--- customer-service/src/main/java/com/microservices/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/microservices/customerservice/CustomerServiceApplication.java
@@ -2,12 +2,15 @@ package com.microservices.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients("com.microservices.customerservice")
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
--- customer-service/src/main/java/com/microservices/customerservice/OrderServiceProxy.java
+++ customer-service/src/main/java/com/microservices/customerservice/OrderServiceProxy.java
@@ -2,20 +2,19 @@ package com.microservices.customerservice;
 
 import java.util.List;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.microservices.customerservice.entity.Order;
 
-@FeignClient(name="order-service",url="localhost:8000")
+//@FeignClient(name="order-service",url="localhost:8001")
+@FeignClient(name="order-service")
+@RibbonClient(name="order-service")
 public interface OrderServiceProxy {
-	
 	
 	@GetMapping("/orders/{customerid}")
 	public List<Order> getOrderByCustomerId(@PathVariable("customerid") Long customerid);
-	
-	
-	
 
 }
