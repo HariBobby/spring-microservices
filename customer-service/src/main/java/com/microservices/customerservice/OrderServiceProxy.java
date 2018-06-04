001aad2aecfce2e63340990845884ca7933529e7
HariBobby <hari.polnati@gmail.com>
2018-06-04 08:51:02 +0530

    Zuul Api gateway configured

--- customer-service/src/main/java/com/microservices/customerservice/OrderServiceProxy.java
+++ customer-service/src/main/java/com/microservices/customerservice/OrderServiceProxy.java
@@ -10,11 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.microservices.customerservice.entity.Order;
 
 //@FeignClient(name="order-service",url="localhost:8001")
-@FeignClient(name="order-service")
+//@FeignClient(name="order-service")
+@FeignClient(name="zuul-api-gateway-server")
 @RibbonClient(name="order-service")
 public interface OrderServiceProxy {
 	
-	@GetMapping("/orders/{customerid}")
+	@GetMapping("/order-service/orders/{customerid}")
 	public List<Order> getOrderByCustomerId(@PathVariable("customerid") Long customerid);
 
 }
---
+++ zuul-api-gateway-server/src/main/java/com/microservices/zuulapigatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,46 @@
+package com.microservices.zuulapigatewayserver;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter{
+	
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+	@Override
+	public Object run() throws ZuulException {
+	
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		
+		LOGGER.info("request -> {} request uri -> {}",request,request.getRequestURI());
+		
+		//LOGGER.info("request -> "+request+"request uri -> {}"+request.getRequestURI());
+		
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+}
