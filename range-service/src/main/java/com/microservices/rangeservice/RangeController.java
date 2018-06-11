f3e752215958570e4bf56c5969f33472adb175ba
HariBobby <hari.polnati@gmail.com>
2018-06-11 09:02:19 +0530

    Hystrix enabled

--- range-service/src/main/java/com/microservices/rangeservice/RangeController.java
+++ range-service/src/main/java/com/microservices/rangeservice/RangeController.java
@@ -1,11 +1,11 @@
 package com.microservices.rangeservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservices.rangeservice.model.RangeConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class RangeController {
@@ -24,5 +24,24 @@ public class RangeController {
 
 		return new RangeConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
+	
+	
+	@GetMapping("/range-fault-tolerance")
+	@HystrixCommand(fallbackMethod="fallbackRetrievelimits")
+	public RangeConfiguration getLimitsfault() {
+		//return new RangeConfiguration(1000, 1);
+//connecting to mahesh machine and pull some data - Remote call
+		throw new RuntimeException("Not available");
+		//return new RangeConfiguration(configuration.getMaximum(), configuration.getMinimum());
+		//return new RangeConfiguration(555551, 551);
+	}
+	
+	
+	public RangeConfiguration fallbackRetrievelimits()
+	{
+		//pull local data
+		return new RangeConfiguration(6666, 66);
+	}
+	
 
 }
